8cf7bb8d4bd2f44469e9c9523a609a6b64b0e7c9
何欣 <shrong.he@gmail.com>
2018-05-09 16:33:43 +0800

    modfiy the nginx name

--- src/core/ngx_log.c
+++ src/core/ngx_log.c
@@ -204,7 +204,7 @@ ngx_log_error_core(ngx_uint_t level, ngx_log_t *log, ngx_err_t err,
 
     msg -= (7 + err_levels[level].len + 3);
 
-    (void) ngx_sprintf(msg, "nginx: [%V] ", &err_levels[level]);
+    (void) ngx_sprintf(msg, "allmedia: [%V] ", &err_levels[level]);
 
     (void) ngx_write_console(ngx_stderr, msg, p - msg);
 }
@@ -264,7 +264,7 @@ ngx_log_stderr(ngx_err_t err, const char *fmt, ...)
 
     last = errstr + NGX_MAX_ERROR_STR;
 
-    p = ngx_cpymem(errstr, "nginx: ", 7);
+    p = ngx_cpymem(errstr, "allmedia: ", 10);
 
     va_start(args, fmt);
     p = ngx_vslprintf(p, last, fmt, args);
